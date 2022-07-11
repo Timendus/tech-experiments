a950ea7d2c41b72422a5bb9a6e380b277fb2f9e1
Tim Franssen <mail@timendus.com>
2022-07-11 15:14:37 +0200

    Move conversations to another file

---
+++ speech-assistant-ui/src/conversations.js
@@ -0,0 +1,87 @@
+module.exports = [
+
+  {
+    meta: {
+      start: -3000,
+      end: 134350
+    },
+    client: {
+      name: "Roald Dijkstra",
+      tags: ["medicatie", "paniek"]
+    },
+    transcript: [
+      {
+        timestamp: -2000,
+        text: "Hallo",
+        caller: true
+      },
+      {
+        timestamp: -1000,
+        text: "Ik wil graag een herhaalrecept aanvragen",
+        caller: true
+      },
+      {
+        timestamp: 3000,
+        text: "Mijn valsartan is op",
+        caller: true
+      },
+      {
+        timestamp: 8000,
+        text: "Mag ik daar een nieuwe van?",
+        caller: true
+      }
+    ]
+  },
+
+  {
+    meta: {
+      start: -167000,
+      end: 134350
+    },
+    client: {
+      name: "Bob van der Linden",
+      tags: ["medicatie", "paniek"]
+    },
+    transcript: [
+      {
+        timestamp: new Date(),
+        text: "Hello world"
+      }
+    ]
+  },
+
+  {
+    meta: {
+      start: -279000,
+      end: 134350
+    },
+    client: {
+      name: "Yannick Donners",
+      tags: ["medicatie", "paniek"]
+    },
+    transcript: [
+      {
+        timestamp: new Date(),
+        text: "Hello world"
+      }
+    ]
+  },
+
+  {
+    meta: {
+      start: 2000,
+      end: 134350
+    },
+    client: {
+      name: "Tim Franssen",
+      tags: ["medicatie", "paniek"]
+    },
+    transcript: [
+      {
+        timestamp: new Date(),
+        text: "Hello world"
+      }
+    ]
+  },
+
+];
